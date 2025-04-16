481aaf3109b72fe61f5b01727aee6245c12f7d92
jauntee <noahwj305@gmail.com>
2025-04-16 00:38:20 -0500

    Added three components, contact-book, contact-form, contact-list, and data folder

---
+++ src/app/contact-book/contact-book.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-book',
+  imports: [],
+  templateUrl: './contact-book.component.html',
+  styleUrl: './contact-book.component.css'
+})
+export class ContactBookComponent {
+
+}
